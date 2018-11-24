e5c1f608fe9dea93bd427d0b47c92dda5e462919
phosfox <mordredur@googlemail.com>
2018-11-24 01:01:26 +0100

    Funktionstüchtig. Will das aber nochmal in OOP umwandeln.

--- Sudoku.php
+++ Sudoku.php
@@ -8,53 +8,14 @@
 
 class Sudoku
 {
-    private $SIZE = 9;
-    private $GRID;
-    private $BOXSIZE = 3;
-
-    /**
-     * Sudoku constructor.
-     * @param $SIZE
-     */
-    public function __construct($SIZE)
+    public static function printSudoku($GRID)
     {
-        $this->SIZE = $SIZE;
-        //$this->BOXSIZE = sqrt($this->SIZE);
-        for ($c = 0; $c < $this->SIZE; $c++) {
-            $this->GRID[$c] = array();
-            for ($r = 0; $r < $this->SIZE; $r++) {
-                $this->GRID[$c][$r] = $r + 1;
+        foreach ($GRID as $row) {
+            foreach ($row as $col) {
+                echo $col . " ";
             }
+            echo "<br />";
         }
-    }
-
-    function printBoard()
-    {
-        for ($c = 0; $c < $this->SIZE; $c++) {
-            for ($r = 0; $r < $this->SIZE; $r++) {
-                echo $this->GRID[$c][$r] . " ";
-                if ($r == 2 or $r == 5) {
-                    print "| ";
-                }
-            }
-            print "<br>";
-
-        }
-    }
-
-    public function createTestBoard()
-    {
-        $this->GRID = array(
-            array(3, 0, 6, 5, 0, 8, 4, 0, 0),
-            array(5, 2, 0, 0, 0, 0, 0, 0, 0),
-            array(0, 8, 7, 0, 0, 0, 0, 3, 1),
-            array(0, 0, 3, 0, 1, 0, 0, 8, 0),
-            array(9, 0, 0, 8, 6, 3, 0, 0, 5),
-            array(0, 5, 0, 0, 9, 0, 6, 0, 0),
-            array(1, 3, 0, 0, 0, 0, 2, 5, 0),
-            array(0, 0, 0, 0, 0, 0, 0, 7, 4),
-            array(0, 0, 5, 2, 0, 6, 3, 0, 0)
-        );
     }
 
     public static function solveBoard($GRID, $n): bool
@@ -78,33 +39,33 @@ class Sudoku
         }
 
         if ($isEmpty) {
+            Sudoku::printSudoku($GRID);
             return true;
         }
 
         for ($num = 1; $num <= $n; $num++) {
             if (Sudoku::isSafe($GRID, $col, $row, $num)) {
-                $GRID[$col][$row] = $num;
+                $GRID[$row][$col] = $num;
                 if (Sudoku::solveBoard($GRID, $n)) {
                     return true;
                 } else {
-                    $GRID[$col][$row] = 0;
+                    $GRID[$row][$col] = 0;
                 }
             }
         }
         return false;
     }
 
-    public
-    static function isSafe($GRID, $col, $row, $num): bool
+    public static function isSafe($GRID, $col, $row, $num): bool
     {
         for ($d = 0; $d < sizeof($GRID); $d++) {
-            if ($GRID[$col][$d] == $num) {
+            if ($GRID[$row][$d] == $num) {
                 return false;
             }
         }
 
         for ($r = 0; $r < sizeof($GRID); $r++) {
-            if ($GRID[$r][$row] == $num) {
+            if ($GRID[$r][$col] == $num) {
                 return false;
             }
         }
@@ -113,8 +74,8 @@ class Sudoku
         $boxRowStart = $row - $row % $boxSize;
         $boxColStart = $col - $col % $boxSize;
 
-        for ($d = $boxColStart; $d < $boxColStart + $boxSize; $d++) {
-            for ($r = $boxRowStart; $r < $boxRowStart + $boxSize; $r++) {
+        for ($r = $boxRowStart; $r < $boxRowStart + $boxSize; $r++) {
+            for ($d = $boxColStart; $d < $boxColStart + $boxSize; $d++) {
                 if ($GRID[$r][$d] == $num) {
                     return false;
                 }
@@ -124,10 +85,6 @@ class Sudoku
 
     }
 
-    public function getGrid()
-    {
-        return $this->GRID;
-    }
 }
 
 
--- hello.php
+++ hello.php
@@ -6,18 +6,24 @@
 <?php
 require 'Sudoku.php';
 # First Example
+$GRID = array(
+    array(3, 0, 6, 5, 0, 8, 4, 0, 0),
+    array(5, 2, 0, 0, 0, 0, 0, 0, 0),
+    array(0, 8, 7, 0, 0, 0, 0, 3, 1),
+    array(0, 0, 3, 0, 1, 0, 0, 8, 0),
+    array(9, 0, 0, 8, 6, 3, 0, 0, 5),
+    array(0, 5, 0, 0, 9, 0, 6, 0, 0),
+    array(1, 3, 0, 0, 0, 0, 2, 5, 0),
+    array(0, 0, 0, 0, 0, 0, 0, 7, 4),
+    array(0, 0, 5, 2, 0, 6, 3, 0, 0)
+);
+$s = new Sudoku($GRID);
+Sudoku::printSudoku($GRID);
 
-$sudoku = new Sudoku(9);
-$GRID = $sudoku->getGrid();
-$sudoku->createTestBoard();
-$sudoku->printBoard();
-echo sizeof($GRID);
-if ($sudoku::solveBoard($GRID, 9)) ;
-{
-    echo "<p>Solution:</p>";
-    $sudoku->printBoard();
-}
+echo "<br>";
+Sudoku::solveBoard($GRID, 9);
 
+echo "ENDE";
 ?>
 </body>
 </html>
